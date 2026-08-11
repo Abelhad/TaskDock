acb2629769c139c48787d6bd4d5226b611d3c717
Abelhad <hassanabelhad51@gmail.com>
2026-08-11 20:23:04 +0100

    add authentication and customize design

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,95 @@
+<x-guest-layout>
+
+    <!-- Session Status -->
+    <x-auth-session-status
+        class="session-status"
+        :status="session('status')"
+    />
+
+    <form method="POST" action="{{ route('login') }}" class="login-form">
+        @csrf
+
+        <!-- Email -->
+        <div class="form-group">
+            <x-input-label
+                for="email"
+                :value="__('Email')"
+                class="form-label"
+            />
+
+            <x-text-input
+                id="email"
+                class="form-input"
+                type="email"
+                name="email"
+                :value="old('email')"
+                required
+                autofocus
+                autocomplete="username"
+            />
+
+            <x-input-error
+                :messages="$errors->get('email')"
+                class="form-error"
+            />
+        </div>
+
+        <!-- Password -->
+        <div class="form-group">
+            <x-input-label
+                for="password"
+                :value="__('Password')"
+                class="form-label"
+            />
+
+            <x-text-input
+                id="password"
+                class="form-input"
+                type="password"
+                name="password"
+                required
+                autocomplete="current-password"
+            />
+
+            <x-input-error
+                :messages="$errors->get('password')"
+                class="form-error"
+            />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="remember-group">
+            <label for="remember_me" class="remember-label">
+                <input
+                    id="remember_me"
+                    type="checkbox"
+                    name="remember"
+                    class="remember-checkbox"
+                >
+
+                <span>Remember me</span>
+            </label>
+        </div>
+
+        <!-- Actions -->
+        <div class="form-actions">
+
+            @if (Route::has('password.request'))
+                <a
+                    href="{{ route('password.request') }}"
+                    class="forgot-link"
+                >
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <button type="submit" class="login-button">
+                {{ __('Log in') }}
+            </button>
+
+        </div>
+
+    </form>
+
+</x-guest-layout>
+
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,103 @@
+<x-guest-layout>
+
+    <form method="POST" action="{{ route('register') }}" class="register-form">
+        @csrf
+
+        <!-- Name -->
+        <div class="form-group">
+            <x-input-label for="name" :value="__('Name')" class="form-label" />
+
+            <x-text-input
+                id="name"
+                class="form-input"
+                type="text"
+                name="name"
+                :value="old('name')"
+                required
+                autofocus
+                autocomplete="name"
+            />
+
+            <x-input-error
+                :messages="$errors->get('name')"
+                class="form-error"
+            />
+        </div>
+
+        <!-- Email -->
+        <div class="form-group">
+            <x-input-label for="email" :value="__('Email')" class="form-label" />
+
+            <x-text-input
+                id="email"
+                class="form-input"
+                type="email"
+                name="email"
+                :value="old('email')"
+                required
+                autocomplete="username"
+            />
+
+            <x-input-error
+                :messages="$errors->get('email')"
+                class="form-error"
+            />
+        </div>
+
+        <!-- Password -->
+        <div class="form-group">
+            <x-input-label for="password" :value="__('Password')" class="form-label" />
+
+            <x-text-input
+                id="password"
+                class="form-input"
+                type="password"
+                name="password"
+                required
+                autocomplete="new-password"
+            />
+
+            <x-input-error
+                :messages="$errors->get('password')"
+                class="form-error"
+            />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="form-group">
+            <x-input-label
+                for="password_confirmation"
+                :value="__('Confirm Password')"
+                class="form-label"
+            />
+
+            <x-text-input
+                id="password_confirmation"
+                class="form-input"
+                type="password"
+                name="password_confirmation"
+                required
+                autocomplete="new-password"
+            />
+
+            <x-input-error
+                :messages="$errors->get('password_confirmation')"
+                class="form-error"
+            />
+        </div>
+
+        <!-- Actions -->
+        <div class="form-actions">
+            <a href="{{ route('login') }}" class="login-link">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="register-button">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+
+    </form>
+
+</x-guest-layout>
+
