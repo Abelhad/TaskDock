69598edeb8d100d931d864ed346d94a41c3d360c
Abelhad <hassanabelhad51@gmail.com>
2026-08-18 02:52:12 +0100

    display tasks stats in all tasks page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,7 +16,30 @@
     @if($tasks->isEmpty())
         <h3>No Tasks Created yet</h3>
     @else
-        test
+        <div class="projectStats">
+
+            <div class="statCard">
+                <span class="statTitle">Total Tasks</span>
+                <h3>{{ $tasks->count() }}</h3>
+                <p>All your Tasks created</p>
+            </div>
+
+            <div class="statCard">
+                <span class="statTitle">Assigned Tasks</span>
+                <h3>{{ $tasks->whereNotNull('assigned_to')->count() }}</h3>
+                <p>Tasks assigned to a team member</p>
+            </div>
+
+            <div class="statCard">
+                <span class="statTitle">Unassigned Tasks</span>
+                <h3>{{ $tasks->whereNull('assigned_to')->count() }}</h3>
+                <p>Tasks waiting to be assigned</p>
+            </div>
+
+
+        </div>
+
+        
     @endif
 
 @endsection
